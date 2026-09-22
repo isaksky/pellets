5fccc5b669af6a2e6fef0b67b2b00e980a13cbe4
Isak Sky <607986+isaksky@users.noreply.github.com>
2026-09-21 18:50:07 -0600

    chore(release): prepare v0.4.0

--- Formula/pl.rb
+++ Formula/pl.rb
@@ -1,17 +1,17 @@
 class Pl < Formula
   desc "Local SQLite task queue for coding agents"
   homepage "https://github.com/isaksky/pellets"
-  version "0.3.0"
+  version "0.4.0"
   license "Apache-2.0"
 
   depends_on :macos
 
   if Hardware::CPU.arm?
-    url "https://github.com/isaksky/pellets/releases/download/v0.3.0/pellets_0.3.0_darwin_arm64.tar.gz"
-    sha256 "c1c66b94e667bc12b76b7471e471aec5287b13d1da99fce443081b64a09c908e"
+    url "https://github.com/isaksky/pellets/releases/download/v0.4.0/pellets_0.4.0_darwin_arm64.tar.gz"
+    sha256 "617c40698ccceb0e63570b36b847f107ad18beade29900e448af7c029593b37b"
   else
-    url "https://github.com/isaksky/pellets/releases/download/v0.3.0/pellets_0.3.0_darwin_amd64.tar.gz"
-    sha256 "2970ca95650d2e094d6568883c9dfb0e4d5ec18599846367adc3506ee1e261d1"
+    url "https://github.com/isaksky/pellets/releases/download/v0.4.0/pellets_0.4.0_darwin_amd64.tar.gz"
+    sha256 "5d86185edd6b40a5c79230c440b249b92f612668641011b68befa4456b3693b2"
   end
 
   def install
